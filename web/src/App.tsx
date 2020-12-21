b00b204923a703ae81b1a9fc83cb26675fd16d97
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-21 19:20:43 +0800

    add route to dashboard

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider, theme } from "@chakra-ui/react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -10,6 +11,9 @@ export const App = () => (
       <Switch>
         <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
         </Route>
         <Route path="/">
           <Home />

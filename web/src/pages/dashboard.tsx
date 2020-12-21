9fa97e8e58a415375377b979397911cd5bfe7b38
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-21 20:55:13 +0800

    use get boards query

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -1,12 +1,27 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { BoardList } from "../components/BoardList";
 import Layout from "../components/Layout";
+import { Board, useGetBoardsQuery } from "../generated/graphql";
 
 const Dashboard: React.FC = () => {
+  const { data } = useGetBoardsQuery();
+  const [boards, setBoards] = useState<Board[]>([]);
+
+  useEffect(() => {
+    if (data?.getBoards) {
+      setBoards(data.getBoards as Board[]);
+    }
+  }, [data]);
   return (
     <Layout>
-      Kanban
-      <Box>create board</Box>
+      <Heading as="h2" size="xl">
+        Boards
+      </Heading>
+
+      <BoardList boards={boards} />
+
+      <Box>Create a board</Box>
     </Layout>
   );
 };

f57269517b134008e9a7a675b430225ccfbf768d
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-21 20:19:37 +0800

    more content on landing page

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,24 +1,25 @@
-import { Link as ChakraLink } from "@chakra-ui/react";
 import React from "react";
+import { Button, Heading, Link as ChakraLink, Stack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { ColorModeSwitcher } from "../components/ColorModeSwitcher";
-import { useMeQuery } from "../generated/graphql";
+import LandingLayout from "../components/LandingLayout";
 
 const Home = () => {
-  const { data } = useMeQuery();
   return (
-    <div>
-      Home
-      <div>Home</div>
-      <div>Home</div>
-      <ColorModeSwitcher />
-      <ChakraLink as={Link} to="/login">
-        hello {data?.me?.email}
-      </ChakraLink>
-      <ChakraLink as={Link} to="/dashboard">
-        Dashboard
-      </ChakraLink>
-    </div>
+    <LandingLayout>
+      <Stack alignItems="center" spacing="40px">
+        <Heading as="h1" size="4xl" isTruncated>
+          Kanban Board
+        </Heading>
+        <Heading as="h2" size="2xl">
+          Organize your projects and tasks
+        </Heading>
+        <ChakraLink as={Link} to="#">
+          <Button variant="solid" colorScheme="green">
+            Create account
+          </Button>
+        </ChakraLink>
+      </Stack>
+    </LandingLayout>
   );
 };
 

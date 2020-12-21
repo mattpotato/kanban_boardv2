c7d10b1c75f9c85fe166aad7ccf5b9934126ba38
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-21 19:21:30 +0800

    create login page

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,76 @@
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Text,
+  Box,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import { ColorModeSwitcher } from "../components/ColorModeSwitcher";
+import { LoginInput, useLoginMutation } from "../generated/graphql";
+
+const Login = () => {
+  const { handleSubmit, reset, register } = useForm();
+  const [login, { data: loginData }] = useLoginMutation();
+  const history = useHistory();
+
+  const onSubmit = (data: LoginInput) => {
+    console.log(data);
+    login({
+      variables: {
+        options: data,
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (loginData) {
+      history.push("/");
+    }
+  }, [loginData, history]);
+
+  return (
+    <div>
+      <ColorModeSwitcher />
+      <Box>hello {loginData?.login.user?.email}</Box>
+
+      <Flex
+        as="form"
+        onSubmit={handleSubmit(onSubmit)}
+        flexDirection="column"
+        width="sm"
+        justifyContent="center"
+      >
+        <FormControl id="email">
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input
+            ref={register}
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+          />
+        </FormControl>
+        <FormControl id="password">
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            ref={register}
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+        </FormControl>
+        <Button variant="solid" colorScheme="green" type="submit">
+          Login
+        </Button>
+
+        <Text>Or Continue with: </Text>
+      </Flex>
+    </div>
+  );
+};
+
+export default Login;

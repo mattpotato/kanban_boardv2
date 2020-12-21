f01d2364b29fe19c3b13386b7f4cea9dcc3bbe73
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-21 19:21:03 +0800

    create dashboard page

---
+++ web/src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Layout from "../components/Layout";
+
+const Dashboard: React.FC = () => {
+  return (
+    <Layout>
+      Kanban
+      <Box>create board</Box>
+    </Layout>
+  );
+};
+
+export default Dashboard;

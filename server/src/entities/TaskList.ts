b4d79459640d17e32ad80f6e332047a9a60a83e4
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-25 20:32:32 +0800

    create tasklist entity

---
+++ server/src/entities/TaskList.ts
@@ -0,0 +1,56 @@
+import { Board } from "./Board";
+import { Field, ObjectType } from "type-graphql";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+  ManyToOne,
+  OneToMany,
+} from "typeorm";
+import { Task } from "./Task";
+
+@ObjectType()
+@Entity()
+export class TaskList extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column()
+  name!: string;
+
+  @Field()
+  @Column()
+  boardId: number;
+
+  @ManyToOne(() => Board, (board) => board.taskLists)
+  board: Board;
+
+  @Field(() => [Task])
+  @OneToMany(() => Task, (task) => task.taskList)
+  tasks: Task[];
+
+  @Field()
+  @Column({ type: "double precision", default: 65535.5 })
+  minPos: number;
+
+  @Field()
+  @Column({ type: "double precision", default: 65535.5 })
+  maxPos: number;
+
+  @Field()
+  @Column({ type: "double precision", default: 65535.5 })
+  pos: number;
+
+  @Field(() => Date)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => Date)
+  @UpdateDateColumn()
+  updatedAt: Date;
+}

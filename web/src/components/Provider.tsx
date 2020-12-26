70cd8ba429d4bd66950828c2298b9a6bee33d192
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-26 17:48:39 +0800

    enable subscriptions

--- web/src/components/Provider.tsx
+++ web/src/components/Provider.tsx
@@ -1,10 +1,43 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 import React from "react";
 
 function Provider({ children }: { children: React.ReactNode }) {
-  const client = new ApolloClient({
+  const httpLink = new HttpLink({
     uri: "http://localhost:4000/graphql",
-    cache: new InMemoryCache({}),
+    credentials: "include",
+  });
+
+  const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+      reconnect: true,
+    },
+  });
+
+  const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      );
+    },
+    wsLink,
+    httpLink
+  );
+
+  const client = new ApolloClient({
+    link: splitLink,
+    // uri: "http://localhost:4000/graphql",
+    cache: new InMemoryCache(),
     credentials: "include",
   });
 

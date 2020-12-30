536ed070ad06b570cc21d6fa0ef75e3219b42274
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-30 22:17:15 +0800

    add logout function

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,13 +1,36 @@
-import { Box } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import React from "react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Link, useHistory } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { useLogoutMutation } from "../generated/graphql";
 
 const Navbar: React.FC = () => {
+  const [logout] = useLogoutMutation();
+  const history = useHistory();
   return (
-    <Box>
-      Kanban
-      <ColorModeSwitcher />
-    </Box>
+    <Flex padding="20px" justifyContent="space-between">
+      <ChakraLink as={Link} to="/dashboard">
+        Kanban
+      </ChakraLink>
+      <Flex>
+        <ColorModeSwitcher />
+        <Button
+          variant="ghost"
+          onClick={() =>
+            logout({
+              update: (_cache, result) => {
+                if (result.data?.logout) {
+                  history.push("/login");
+                }
+              },
+            })
+          }
+        >
+          Logout
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 

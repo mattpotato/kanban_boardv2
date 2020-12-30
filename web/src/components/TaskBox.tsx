0fb38218632b12c78d39c78e88944f4d7c20a720
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-30 16:38:26 +0800

    add taskbox hover options

--- web/src/components/TaskBox.tsx
+++ web/src/components/TaskBox.tsx
@@ -1,6 +1,7 @@
-import { Box } from "@chakra-ui/react";
+import { Box, IconButton } from "@chakra-ui/react";
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
+import { BsThreeDots } from "react-icons/bs";
 import { Task } from "../generated/graphql";
 
 interface TaskBoxProps {
@@ -18,6 +19,8 @@ const TaskBox: React.FC<TaskBoxProps> = React.memo(({ data, index }) => {
           {...provided.dragHandleProps}
         >
           <Box
+            role="group"
+            position="relative"
             width="300px"
             borderRadius="3px"
             borderColor="rgba(255, 255, 255, 0.1)"
@@ -28,6 +31,20 @@ const TaskBox: React.FC<TaskBoxProps> = React.memo(({ data, index }) => {
             _hover={{ borderColor: "black" }}
           >
             {data.name}
+            <IconButton
+              icon={<BsThreeDots />}
+              aria-label="options"
+              position="absolute"
+              visibility="hidden"
+              opacity="0"
+              top="2px"
+              right="0px"
+              _groupHover={{
+                visibility: "visible",
+                opacity: "1",
+                transition: "all 0.2s ease-in-out",
+              }}
+            />
           </Box>
         </div>
       )}

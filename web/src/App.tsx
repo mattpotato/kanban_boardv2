96cdbd1ae9cf30790750dba49f3cb7f22d43d5e2
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-31 16:52:55 +0800

    add redirects

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,40 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 import Board from "./pages/board";
 import Register from "./pages/register";
+import { useMeQuery } from "./generated/graphql";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Router>
-      <Switch>
-        <Route path="/board/:boardId" component={Board} />
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  </ChakraProvider>
-);
+export const App = () => {
+  const { data } = useMeQuery();
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/board/:boardId" component={Board} />
+          <Route path="/login">
+            {data?.me ? <Redirect to="/dashboard" /> : <Login />}
+          </Route>
+          <Route path="/register">
+            {data?.me ? <Redirect to="/dashboard" /> : <Register />}
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </ChakraProvider>
+  );
+};

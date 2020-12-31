096ab9e84b6b46c0b1cb65fe8d69194cc9bee321
matt011 <38515217+matt011@users.noreply.github.com>
2020-12-31 17:08:31 +0800

    fix logout bug

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -4,12 +4,10 @@ import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Link, useHistory } from "react-router-dom";
 import { Link as ChakraLink } from "@chakra-ui/react";
 import { useLogoutMutation } from "../generated/graphql";
-import { useApolloClient } from "@apollo/client";
 
 const Navbar: React.FC = () => {
   const [logout] = useLogoutMutation();
-  const history = useHistory();
-  const client = useApolloClient();
+
   return (
     <Flex padding="20px" justifyContent="space-between">
       <ChakraLink as={Link} to="/dashboard">
@@ -21,11 +19,11 @@ const Navbar: React.FC = () => {
           variant="ghost"
           onClick={() =>
             logout({
-              update: (_cache, result) => {
+              update: async (cache, result) => {
                 if (result.data?.logout) {
-                  history.push("/login");
+                  await cache.reset();
+                  window.location.replace("/login");
                 }
-                client.resetStore();
               },
             })
           }

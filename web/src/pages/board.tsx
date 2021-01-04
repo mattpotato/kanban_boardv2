00c18827a801d86f2985da3b7587b5d375c2b560
matt011 <38515217+matt011@users.noreply.github.com>
2021-01-04 23:35:51 +0800

    add temporary print

--- web/src/pages/board.tsx
+++ web/src/pages/board.tsx
@@ -307,6 +307,7 @@ const Board: React.FC<RouteComponentProps<BoardRouteInfo>> = (props) => {
 
   useEffect(() => {
     if (activityData?.onNewActivity) {
+      console.log("new activity data");
       client.cache.evict({ fieldName: "getBoardById" });
     }
   }, [activityData, client]);
